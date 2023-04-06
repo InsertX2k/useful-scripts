1bef8be3e9f45137dffe3592de9bb16ac332d9fe
Ziad <62176660+InsertX2k@users.noreply.github.com>
2023-04-06 21:56:47 +0200

    Add: Script file for managing Chrome's Incognito mode
    This is a script file for managing Google Chrome's Incognito mode.

---
+++ manage_incognito_mode_gchrome.bat
@@ -0,0 +1,113 @@
+@echo off
+:init
+    setlocal DisableDelayedExpansion
+    set "batchPath=%~0"
+    for %%k in (%0) do set batchName=%%~nk
+    set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+    setlocal EnableDelayedExpansion
+
+:checkPrivileges
+    NET FILE 1>NUL 2>NUL
+    if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+    if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+    echo args = "ELEV " >> "%vbsGetPrivileges%"
+    echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+    echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+    echo Next >> "%vbsGetPrivileges%"
+    echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+    "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+    exit /B
+
+:gotPrivileges
+    setlocal & pushd .
+    cd /d %~dp0
+    if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+    goto main
+
+
+
+
+:main
+cls
+title Manage incognito mode - Google Chrome
+echo ==================================
+echo Manage Chrome's Incognito Mode
+echo ==================================
+echo.
+echo This involves some registry editing, which will require administrative privileges.
+echo.
+echo Please choose what do you want to do to Chrome's built-in incognito mode:
+echo.
+echo 1-Disable Chrome's incognito mode.
+echo 2-Enable Chrome's incognito mode.
+echo 3-Exit this script.
+echo.
+choice /c 123 /M "Choose an option: "
+if %errorlevel% == 1 goto disable
+if %errorlevel% == 2 goto enable
+if %errorlevel% == 3 goto quit
+rem abnormal exiting...
+exit /b 25 
+
+:disable
+    cls
+    rem disable chrome's incognito mode.
+    echo.
+    echo Disable chrome's incognito mode.
+    echo.
+    echo Step #1 - Create google policies registry key.
+    echo Creating google policies registry key...
+    echo.
+    rem creates policies/google registry key
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" /f
+    echo Step #1 done, continuing to Step #2.
+    echo Step #2 - Creating chrome registry key in Google's registry key...
+    echo.
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /f
+    echo Step #2 done, continuing to step #3.
+    echo Step #3 - Creating the "IncognitoModeAvailability" registry value...
+    echo.
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "IncognitoModeAvailability" /t REG_DWORD /d 1 /f
+    echo Steps #1,#2,#3 done.
+    echo Disabled Chrome's Incognito Mode.
+    echo.
+    rem all done prompt
+    echo Everything is done, press any key to close this window...
+    pause >nul
+    exit /b 0
+
+:enable
+    cls
+    rem enable chrome's incognito mode.
+    echo.
+    echo Enable chrome's incognito mode.
+    echo.
+    echo Step #1, delete the Chrome policies registry key.
+    echo Deleting Chrome policies registry key...
+    echo.
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" /f
+    echo.
+    echo Step #1 done.
+    echo.
+    rem all done prompt
+    echo Everything is done, press any key to close this window...
+    pause >nul
+    exit /b 0
+
+
+:quit
+    rem when error level is not 1 or 2
+    rem when the user chooses 3 to exit this script file.
+    cls
+    echo.
+    echo User has chosen to exit this script file.
+    echo Press any key to close this window...
+    pause >nul
+    exit /b 0
+
+
+rem abnormal exiting.
+exit /b 25
